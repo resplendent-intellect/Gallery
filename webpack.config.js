7bcd551dea3497f236a3a4f8fd7014d455d0a453
hayley jones <joneshayley991@gmail.com>
2021-01-20 11:52:12 -0800

    app renders to the page

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,54 @@
 const path = require('path');
-const DIST_DIR = path.join(__dirname, '/client')
+
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
-  mode: 'development',
-  entry: './client/src/index.jsx',
+  entry: `${SRC_DIR}/index.jsx`,
   output: {
-    path: path.resolve(DIST_DIR, 'dist'),
     filename: 'bundle.js',
+    path: DIST_DIR,
   },
   module: {
-    rules: [
+    loaders: [
       {
-        test: /\.jsx?$/,
-        include: [
-          path.resolve(__dirname, 'src'),
-        ],
+        test: /\.jsx?/,
+        include: SRC_DIR,
         loader: 'babel-loader',
-        options: {
+        query: {
           presets: ['react', 'es2015'],
         },
       },
     ],
   },
 };
+
+// const path = require('path');
+
+// const DIST_DIR = path.join(__dirname, '/client');
+
+// module.exports = {
+//   mode: 'development',
+//   entry: './client/src/index.jsx',
+//   output: {
+//     path: path.resolve(DIST_DIR, 'dist'),
+//     filename: 'bundle.js',
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.jsx?$/,
+//         exclude: /node_modules/,
+//         include: [
+//           path.resolve(__dirname, 'src'),
+//         ],
+//         use: {
+//           loader: 'babel-loader',
+//           options: {
+//             presets: ['@babel/preset-env'],
+//           },
+//         },
+//       },
+//     ],
+//   },
+// };
