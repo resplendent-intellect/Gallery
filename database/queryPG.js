e993022f21416ce5eddd89e0ccdc487b1a72f7e3
Scotty Cayton <kcayton34@gmail.com>
2021-02-10 17:09:14 -0700

    Updated queries to utilize prepared statements. Now I'm really moving to EC2.

--- database/queryPG.js
+++ database/queryPG.js
@@ -1,18 +1,18 @@
 const pool = require('./indexPG.js');
 
-const mainQueryText = 'SELECT * FROM product_info NATURAL JOIN answered_questions NATURAL JOIN reviews NATURAL JOIN sku_info WHERE product_id =';
+const mainQueryText = 'SELECT * FROM product_info NATURAL JOIN answered_questions NATURAL JOIN reviews NATURAL JOIN sku_info WHERE product_id = $1';
 
-const skuQueryText = 'SELECT * FROM sku_info WHERE product_id =';
+const skuQueryText = 'SELECT * FROM sku_info WHERE product_id = $1';
 
-const productQuery = (productNum, queryLine, analyze = false) => {
+const productQuery = (queryLine, analyze = false) => {
   const preState = analyze ? 'EXPLAIN ANALYZE' : '';
-  const queryText = `${preState} ${queryLine} ${productNum}`;
+  const queryText = `${preState} ${queryLine}`;
   return queryText;
 };
 
 function productInfo(productNum, analyze = false) {
   return new Promise((resolve, reject) => {
-    pool.query(productQuery(productNum, mainQueryText, analyze))
+    pool.query(productQuery(mainQueryText, analyze), [productNum])
       .then((result) => resolve(result))
       .catch((err) => reject(err));
   });
@@ -20,16 +20,16 @@ function productInfo(productNum, analyze = false) {
 
 function skuInfo(skuNum, analyze = false) {
   return new Promise((resolve, reject) => {
-    pool.query(productQuery(skuNum, skuQueryText, analyze))
+    pool.query(productQuery(skuQueryText, analyze), [skuNum])
       .then((result) => resolve(result))
       .catch((err) => reject(err));
   });
 }
 
 // uncomment below for testing queries w/out API.
-productInfo(11000000, true)
-  .then((result) => console.log(result))
-  .catch((err) => console.log(err.stack));
+// productInfo(12, true)
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err.stack));
 
 module.exports = {
   productInfo,
